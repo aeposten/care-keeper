1cff9686c573ab454c2d2dd9f53ba6af6c2e904e
Shannon Sorensen <sorensen.sms@gmail.com>
2021-10-25 11:32:57 -0500

    added imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
+import React, {useState, useEffect} from "react"
+import Header from "./Header";
+import Navbar from "./Navbar";
+import PetList from "./PetList";
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+    .then(response => response.json())
+    .then(data => console.log(data))
+  })
   return (
     <div className="App">
-      <header className="App-header"></header>
+      < Header />
+      < Navbar />
+      < PetList />
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './Search'
+import PetForm from './PetForm'
+
+export default function Navbar() {
+    return (
+        <div>
+            < Search />
+            < PetForm />
+        </div>
+    )
+}
