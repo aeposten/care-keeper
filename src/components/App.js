a69304c671c750563d531517f36ae6e6a1a2b0e9
Amy Posten <amyposten85@gmail.com>
2021-10-26 10:34:41 -0400

    AaMade a few edits, got json working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import PetList from "./PetList";
-import Header from "./Header"
+import Header from "./Header";
 
 function App() {
+  const [allPets, setAllPets] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/pets")
+      .then((response) => response.json())
+      .then((allPets) => {
+        setAllPets(allPets);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header"><Header /></header>
+      <header className="App-header">
+        <Header />
+      </header>
       <PetList />
     </div>
   );
