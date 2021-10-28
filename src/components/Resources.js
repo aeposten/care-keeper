16edf7f11feda821af85b9d32215674696bcb04b
Shannon Sorensen <sorensen.sms@gmail.com>
2021-10-28 16:07:07 -0500

    more details in resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,12 +1,15 @@
 const Resources = () => {
   return (
     <>
-      <h1>Resources</h1>
-      <ul>
-        <li> <a href="https://www.aspca.org/pet-care/animal-poison-control"> ASPCA Animal Poison Control </a></li>
-        <li> <a href="https://www.aaha.org/your-pet/pet-microchip-lookup/microchip-search/"> Pet Microchip Lookup </a></li>
-        <li> <a href="https://www.avma.org/resources/pet-owners/emergencycare/first-aid-tips-pet-owners"> Pet First Aid</a> </li>
-      </ul>    
+      <h2><u>Resources</u></h2>
+      <dl>
+        <dt> <a href="https://www.aspca.org/pet-care/animal-poison-control"> ASPCA Animal Poison Control </a></dt>
+        <dd> - Did your client's pet eat something potentially dangerous substance or got themselves into an unexpected (and possibly dangerous) snack? Check this database to see various foods, plants, and medications that are toxic to dogs, cats, and other pets. If you are concerned, please call (888) 426-4435 (a consultation fee may apply) </dd>
+        <dt> <a href="https://www.aaha.org/your-pet/pet-microchip-lookup/microchip-search/"> Pet Microchip Lookup </a></dt>
+        <dd> - Have you found a stray, or are making sure your client's pet is up-to-date with their microchip information? Type the serial number into this database to make sure everything us up to snuff. </dd>
+        <dt> <a href="https://www.avma.org/resources/pet-owners/emergencycare/first-aid-tips-pet-owners"> Pet First Aid</a> </dt>
+        <dd> - Emergencies do happen. See what you can do to provide aid to an unjured pet when veterinary care is not immediately available. Not to be used in place of a liscened veterinary professional. </dd>
+      </dl>    
     </>
   );
 };
