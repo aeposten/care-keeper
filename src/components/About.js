813732a0622132548b83d26ae2ca445faa688628
Shannon Sorensen <sorensen.sms@gmail.com>
2021-10-28 14:21:46 -0500

    added resources and about

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,7 @@ const About = () => {
     <>
       <h1>About</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Quam lacus
-        suspendisse faucibus interdum posuere lorem. Nibh nisl condimentum id
-        venenatis. Non arcu risus quis varius quam quisque id diam. Elit
-        pellentesque habitant morbi tristique. Tellus pellentesque eu tincidunt
-        tortor aliquam nulla facilisi. Turpis in eu mi bibendum neque egestas
-        congue quisque egestas.
+        Care Keeper is a top-rate application for pet-sitters, designed with the necessities of pet care in mind. The app was created by two coding students with a personal history in the veterinary field - Amy was a technician, and Shannon was both a receptionist and a veterinary assistant. While no longer in that field, both Amy and Shannon still have a big ol' soft spot in their hearts for all pets, especially their own. Those behind Care Keeper know that it's important to clearly store and organize the information for your pet-sitting clients. 
       </p>
     </>
   );
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -2,15 +2,11 @@ const Resources = () => {
   return (
     <>
       <h1>Resources</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Quam lacus
-        suspendisse faucibus interdum posuere lorem. Nibh nisl condimentum id
-        venenatis. Non arcu risus quis varius quam quisque id diam. Elit
-        pellentesque habitant morbi tristique. Tellus pellentesque eu tincidunt
-        tortor aliquam nulla facilisi. Turpis in eu mi bibendum neque egestas
-        congue quisque egestas.
-      </p>
+      <ul>
+        <li> <a href="https://www.aspca.org/pet-care/animal-poison-control"> ASPCA Animal Poison Control </a></li>
+        <li> <a href="https://www.aaha.org/your-pet/pet-microchip-lookup/microchip-search/"> Pet Microchip Lookup </a></li>
+        <li> <a href="https://www.avma.org/resources/pet-owners/emergencycare/first-aid-tips-pet-owners"> Pet First Aid</a> </li>
+      </ul>    
     </>
   );
 };
